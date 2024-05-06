b3004d04d587fd5cbca9fc4fd20cdd43d0b12c75
RoyElias <roy.e.elias@gmail.com>
2024-05-06 23:34:55 +0300

    end of lesson 5

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,9 @@ import AcmeLogo from '@/app/ui/acme-logo';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 
+import styles from '@/app/ui/home.module.css';
+
+
 export default function Page() {
   return (
     <main className="flex min-h-screen flex-col p-6">
@@ -28,6 +31,8 @@ export default function Page() {
           {/* Add Hero Images Here */}
         </div>
       </div>
+      <div className={styles.shape} />
+      <div className="h-0 w-0 border-b-[30px] border-l-[20px] border-r-[20px] border-b-black border-l-transparent border-r-transparent"/>
     </main>
   );
 }
